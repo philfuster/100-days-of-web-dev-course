3c850cb2a485263396dd2daf547b4c16d4ef4c81
Philip Fuster <paf@storis.com>
2022-08-02 13:33:29 -0400

    Finished Section-28: AJAX & Asynchronous JS-driven HTTP Requests

---
+++ dev/section-28/fetch-demo/public/scripts/comments.js
@@ -0,0 +1,73 @@
+const loadCommentsBtnElement = document.getElementById('load-comments-btn');
+const commentsElement = document.getElementById('comments');
+const commentsFormElement = document.querySelector('#comments-form form');
+const commentTitleElement = document.getElementById('title');
+const commentTextElement = document.getElementById('text');
+
+function createCommentList(comments) {
+  const commentListElement = document.createElement('ol');
+
+  comments.forEach(comment => {
+    const commentElement = document.createElement('li');
+    commentElement.innerHTML = `
+      <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+      </article>
+    `;
+    commentListElement.appendChild(commentElement);
+  })
+  return commentListElement;
+}
+
+async function fetchCommentsForPost(event) {
+  const postId = loadCommentsBtnElement.dataset.postid;
+  try {
+    if (!response.ok) {
+      alert('Fetching comments failed!');
+      return;
+    }
+    const response = await fetch(`/posts/${postId}/comments`);
+    const responseData = await response.json();
+    if (responseData != null && responseData.length > 0) {
+      commentsElement.innerHTML = '';
+      commentsElement.appendChild(createCommentList(responseData));
+    } else {
+      commentsElement.firstElementChild.textContent = 'No comments found';
+    }
+  } catch (error) {
+    alert('Error Fetching comments!')
+  }
+}
+
+async function saveComment(event) {
+  event.preventDefault();
+  const postId = commentsFormElement.dataset.postid;
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+
+  const comment = {
+    title: enteredTitle,
+    text: enteredText,
+  }
+
+  try {
+    const response = await fetch(`/posts/${postId}/comments`, {
+      method: 'POST',
+      body: JSON.stringify(comment),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    if (response.ok) {
+      fetchCommentsForPost();
+    } else {
+      alert('could not send comment!');
+    }
+  } catch(error) {
+    alert('Could not send request - maybe try again later!');
+  }
+}
+
+loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
+commentsFormElement.addEventListener('submit', saveComment);
