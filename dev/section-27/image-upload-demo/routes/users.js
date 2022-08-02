bdcef3059fe9273e208159532c18df44d7c86c49
paf334 <paf@storis.com>
2022-08-02 00:43:51 -0400

    Finished Section-27 - about uploading files/images and storing them on the server.
    Starting up section-28 - about the concept of ajax.

---
+++ dev/section-27/image-upload-demo/routes/users.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const multer = require("multer");
+
+const db = require("../data/database");
+
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
+const router = express.Router();
+
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users });
+});
+
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
+});
+
+router.post("/profiles", upload.single("image"), async function (req, res) {
+  const { file: uploadedImageFile } = req;
+  const { username } = req.body;
+  await db.getDb().collection("users").insertOne({
+    name: username,
+    imagePath: uploadedImageFile.path,
+  });
+  res.redirect("/");
+});
+
+module.exports = router;
